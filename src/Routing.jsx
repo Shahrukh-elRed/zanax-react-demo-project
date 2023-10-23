34575a529f9921fad013ca539041d3bb71ece301
Shahrukh <shahrukh1494@gmail.com>
2023-10-23 12:35:56 +0530

    created basic folder structure

---
+++ src/Routing.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Routing = () => {
+    return (
+        <Router>
+            <Routes>
+                {/* <Route path="/" element={<Dashboard />} /> */}
+            </Routes>
+        </Router >
+    )
+}
+
+export default Routing;
