ad59fe45ba2ad306c81afa9cdd06d1a043fadc9c
Shahrukh <shahrukh1494@gmail.com>
2023-10-26 11:37:47 +0530

    fixed accordion opening only one at a time

--- src/Components/FaqAccordion/FaqAccordion.jsx
+++ src/Components/FaqAccordion/FaqAccordion.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import FaqAccordionSingleCard from "../FaqAccordionSingleCard/FaqAccordionSingleCard";
 import "./faq-accordion.scss";
 
 const FaqAccordion = ({ title, data }) => {
+    const [openAccordion, setOpenAccordion] = useState(0);
+
+    const handleAccordionClick = (index) => {
+        if (index !== openAccordion) {
+            setOpenAccordion(index);
+         } else {
+            setOpenAccordion(null);
+        }
+      };
+
     return (
         <div className="card">
             <div className="card-header">
@@ -9,7 +20,8 @@ const FaqAccordion = ({ title, data }) => {
             </div>
             <div className="card-body">
                 <div className="accordion" id="accordionExample">
-                    {data.map(item => <FaqAccordionSingleCard key={item.question.id} question={item.question} answer={item.answer} />)}
+                    {data.map(item => <FaqAccordionSingleCard key={item.question.id} question={item.question} answer={item.answer} 
+                     handleAccordionClick={handleAccordionClick} openAccordion={openAccordion} /> )}
                 </div>
             </div>
         </div>
--- src/Components/FaqAccordionSingleCard/FaqAccordionSingleCard.jsx
+++ src/Components/FaqAccordionSingleCard/FaqAccordionSingleCard.jsx
@@ -1,18 +1,13 @@
-import { useState } from "react";
-
-const FaqAccordionSingleCard = ({ question, answer }) => {
-    const [isCollapsed, setIsCollapsed] = useState(true);
-    const setOpen = () => setIsCollapsed(prev => !prev);
-
+const FaqAccordionSingleCard = ({ question, answer, handleAccordionClick, openAccordion }) => {
     return (
         <>                
             <div className="accordion-item">
-                <h2 className="accordion-header" id={question.id} onClick={setOpen}>
-                    <button className={`${isCollapsed ? "accordion-button collapsed" : "accordion-button"}`} type="button" data-bs-toggle="collapse" data-bs-target={`#${answer.id}`} aria-expanded={!isCollapsed} aria-controls={answer.id}>
+                <h2 className="accordion-header" id={question.id} onClick={() => handleAccordionClick(question.id)}>
+                    <button className={`${openAccordion !== question.id ? "accordion-button collapsed" : "accordion-button"}`} type="button" data-bs-toggle="collapse" data-bs-target={`#${answer.id}`} aria-expanded={openAccordion === question.id} aria-controls={answer.id}>
                         {question.questionText}
                         </button>
                 </h2>
-                <div id={answer.id} className={`${isCollapsed ? "accordion-collapse collapse" : "accordion-collapse collapse show"}`} aria-labelledby={question.id} data-bs-parent="#accordionExample">
+                <div id={answer.id} className={`${openAccordion !== question.id ? "accordion-collapse collapse" : "accordion-collapse collapse show"}`} aria-labelledby={question.id} data-bs-parent="#accordionExample">
                     <div className="accordion-body">
                         {answer.answerText}
                     </div>
