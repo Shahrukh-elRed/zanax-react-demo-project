749929c6417ee4789dfd76eef41d192cb80c1c4c
Shahrukh <shahrukh1494@gmail.com>
2023-11-23 11:09:50 +0530

    fixed image about cards buttons placement

--- src/Pages/AboutCompany/ImageAboutCards/ImageAboutCards.jsx
+++ src/Pages/AboutCompany/ImageAboutCards/ImageAboutCards.jsx
@@ -11,7 +11,7 @@ const ImageAboutCards = ({ imageUrl, header, description, link, buttonColor }) =
             <div className="card-body">
                 <h3 className="header-img-card">{header}</h3>
                 <p className="img-card-text">{description}</p>
-                <button onClick={() => navigate(link)} className={`btn btn-${buttonColor}`}>-Read More</button>
+                <div className="img-card-btn"><button onClick={() => navigate(link)} className={`btn btn-${buttonColor}`}>-Read More</button></div>
             </div>
         </div>
     </div>
